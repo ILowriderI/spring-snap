2432b999081e1260d0e95f51103aab9eab7093eb
ILowriderI <126571170+ILowriderI@users.noreply.github.com>
2023-07-07 19:20:35 +0300

    finished logout mechanism

--- src/main/java/com/example/myproject/cofigurations/JwtAuthenticationFilter.java
+++ src/main/java/com/example/myproject/cofigurations/JwtAuthenticationFilter.java
@@ -1,6 +1,7 @@
 package com.example.myproject.cofigurations;
 
 
+import com.example.myproject.repositories.TokenRepository;
 import com.example.myproject.services.JwtService;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
@@ -21,11 +22,9 @@ import java.io.IOException;
 @Component
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
-
-
     private final JwtService jwtService;
     private final UserDetailsService userDetailsService;
-
+    private  final TokenRepository tokenRepository;
     @Override
     protected void doFilterInternal(
             @NonNull HttpServletRequest request,
@@ -43,7 +42,10 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         userEmail = jwtService.extractUsername(jwt);
         if (userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null) {
             UserDetails userDetails = this.userDetailsService.loadUserByUsername(userEmail);
-            if (jwtService.isTokenValid(jwt, userDetails)) {
+            var isTokenValid = tokenRepository.findByToken(jwt)
+                    .map(token ->!token.isExpired() && !token.isRevoked() )
+                    .orElse(false);
+            if (jwtService.isTokenValid(jwt, userDetails) && isTokenValid) {
                 UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
                         userDetails,
                         null,
